8b3971f27005b088a2ba17e0618ae1d2cbae9029
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2017-03-04 22:18:09 -0500

    update

--- scrape.py
+++ scrape.py
@@ -8,7 +8,7 @@ import csv
 url = 'http://rirs3.royin.go.th/coinages/websearch.php?Wd=&Mrs={max_results}&Sj=0&Ot=2&Cpr={i}&Pnt={i}'
 headers = {'Referer': 'http://rirs3.royin.go.th/coinages/webfind.php'}
 max_results = 109152
-start = 10
+start = 55970
 session = requests.Session()
 generator = (grequests.get(url.format(max_results=max_results, i=i), headers=headers)
              for i in range(start, max_results, 10))
@@ -21,14 +21,14 @@ for page in pages:
         print('have error in encoding; please check')
         content_tmp = page.content.decode('TIS-620', 'replace')
     content = content_tmp.replace('', '“').replace('', '”').replace('', '‘').replace('', '’')
-    soup = BeautifulSoup(content.replace('<br>', '<br />'), 'html5lib')
+    soup = BeautifulSoup(content, 'html5lib')
     lst = soup.select('table[border="1"]')[0].tbody.contents[1:]
     lst = list(filter(lambda x: x.name == 'tr', lst))
     print(len(lst))
     for row in lst:
         src, dst, reference = filter(lambda x: x.name == 'td', row.contents)
         source, year = reference.font.stripped_strings
-        with open('file-{}.csv'.format(source), 'a') as f:
+        with open('file-{}.tsv'.format(source), 'a') as f:
             wr = csv.writer(f, delimiter='\t')
-            wr.writerow([src.text, dst.text, year])
+            wr.writerow([''.join(str(x) for x in src.contents), ''.join(str(x) for x in dst.contents), year])
     page.close()
